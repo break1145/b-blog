0f59667b73080129dcf99febf9e0db47a4570607
break1145 <breakgoogle@163.com>
2025-05-25 14:00:05 +0800

    [feat] User实体及其CRUD

---
+++ src/main/java/com/bblog/repository/UserRepository.java
@@ -0,0 +1,7 @@
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
